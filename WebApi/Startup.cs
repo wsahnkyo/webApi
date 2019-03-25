bd89860dc77c4414addf84b68d791060f2fe1584
anhaonan <1659230515@qq.com>
2019-03-25 12:04:04 +0800

    跨域

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -47,7 +47,7 @@ namespace WebApi
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors(builder => builder.AllowAnyOrigin());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
